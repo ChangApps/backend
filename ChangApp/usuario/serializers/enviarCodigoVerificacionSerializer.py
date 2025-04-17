c0fada95c19a0bd8a0d97bb5c7a7c13ba0601dcc
Elias <elias63856@gmail.com>
2025-04-16 23:21:56 -0300

    Descripción de los cambios realizados

--- ChangApp/usuario/serializers/enviarCodigoVerificacionSerializer.py
+++ ChangApp/usuario/serializers/enviarCodigoVerificacionSerializer.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.conf import settings
 from django.core.cache import cache
 from random import randint
+
+User = get_user_model()
 
 
 class EnviarCodigoVerificacionSerializer(serializers.Serializer):
