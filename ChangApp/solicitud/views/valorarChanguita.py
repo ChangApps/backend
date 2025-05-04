e3b2c9d355b16d408c4f2ba86ba099d9d263f371
unknown <yatenbueno@gmail.com>
2025-05-04 18:44:21 -0300

    views y funciones de gestion de changuitas entre usuarios finalizadas (Back)

--- ChangApp/solicitud/views/valorarChanguita.py
+++ ChangApp/solicitud/views/valorarChanguita.py
@@ -1,4 +1,4 @@
-from datetime import timezone
+from django.utils import timezone
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
