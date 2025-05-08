d95e930e766ece8e0732cc80aaae4f7b3478f174
Martin Gutierrez <martinmgutierrez2003@gmail.com>
2025-05-08 20:23:37 -0300

    elimino user_id_view y arreglo

--- ChangApp/usuario/views/user_id_view.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def user_id_view(request):
-    return Response({'id': request.user.id})
