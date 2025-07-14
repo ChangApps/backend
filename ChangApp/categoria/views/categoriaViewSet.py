cccb12c9962c333bb427193cdc7863fd9c13bd69
ElyasM10 <eliasmaldonado445@gmail.com>
2025-07-14 12:55:06 -0300

    Se agrego endpoint para obtener las subcategorias de una categoria

--- ChangApp/categoria/views/categoriaViewSet.py
+++ ChangApp/categoria/views/categoriaViewSet.py
@@ -3,6 +3,7 @@ from ChangApp.categoria.models import Categoria
 from rest_framework.response import Response
 from rest_framework import viewsets
 from ChangApp.categoria.serializers.categoriaSerializers import CategoriaSerializer
+from rest_framework.decorators import action
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
@@ -15,3 +16,9 @@ class CategoriaViewSet(viewsets.ModelViewSet):
             self.perform_create(serializer)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @action(detail=True, methods=['get'], url_path='subcategorias')
+    def obtener_subcategorias(self, request, pk=None):
+        subcategorias = Categoria.objects.filter(categoria_padre_id=pk)
+        serializer = self.get_serializer(subcategorias, many=True)
+        return Response(serializer.data)
